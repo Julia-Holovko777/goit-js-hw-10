f829972deee7cd7789e7cf8bd4e74a04857666f3
Julia-Holovko777 <golovko.j27@gmail.com>
2023-12-05 22:33:23 +0200

    add cat_api

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,30 +1,69 @@
 import axios from 'axios';
-axios.defaults.headers.common['x-api-key'] =
-  'live_OqaSbYfRny3ZVMd8ZLXbMIV92lYgGf6BhGOQq6yCaoQQQend9qK2WhD67arJpv5L';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import '/node_modules/slim-select/dist/slimselect.css';
 
-const BASE_URL = 'https://api.thecatapi.com/v1';
-const API_KEY = '[x-api-key]';
-// 'live_OqaSbYfRny3ZVMd8ZLXbMIV92lYgGf6BhGOQq6yCaoQQQend9qK2WhD67arJpv5L';
-
-function fetchBreeds() {
-  return axios
-    .get(`${BASE_URL}/breeds?${API_KEY}`)
-    .then(responce => {
-      return responce.data;
-    })
-    .catch(error => {
-      throw new Error(error);
+export async function fetchBreeds(selectEl, loadingEl, errorEl) {
+  try {
+    const response = await axios.get(`https://api.thecatapi.com/v1/breeds`);
+    loadingEl.style.display = 'none';
+    response.data.forEach(element => {
+      const optionEl = document.createElement('option');
+      optionEl.value = element.id;
+      optionEl.textContent = element.name;
+      selectEl.append(optionEl);
     });
+    new SlimSelect({
+      select: '#selectElement',
+    });
+  } catch (error) {
+    loadingEl.style.display = 'none';
+    selectEl.style.display = 'none';
+    errorEl.style.display = 'block';
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+      {
+        position: 'center-center',
+        timeout: 5000,
+        with: '500px',
+        fontSize: '24px',
+      }
+    );
+    throw new Error(error);
+  }
 }
 
-function fetchCatByBreed(breedId) {
-  return axios
-    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-    .then(response => response.data)
-    .catch(error => {
-      console.log(error);
-      throw error;
-    });
+export async function fetchCatByBreed(
+  selectedBreedId,
+  selectEl,
+  loadingEl,
+  errorEl
+) {
+  try {
+    const response = await axios.get(
+      `https://api.thecatapi.com/v1/images/search?breed_ids=${selectedBreedId}`
+    );
+    const item = response.data[0];
+    const breedData = item.breeds[0];
+    return {
+      name: breedData.name,
+      description: breedData.description,
+      temperament: breedData.temperament,
+      imageUrl: item.url,
+    };
+  } catch (error) {
+    loadingEl.style.display = 'none';
+    selectEl.style.display = 'none';
+    errorEl.style.display = 'block';
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+      {
+        position: 'center-center',
+        timeout: 5000,
+        with: '500px',
+        fontSize: '24px',
+      }
+    );
+    throw error;
+  }
 }
-
-export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-import SlimSelect from 'slim-select';
-import Notiflix from 'notiflix';
-import './css/styles.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_OqaSbYfRny3ZVMd8ZLXbMIV92lYgGf6BhGOQq6yCaoQQQend9qK2WhD67arJpv5L';
+
+const selectEl = document.querySelector('.breed-select');
+const loadingEl = document.querySelector('.loader');
+const catInfoEl = document.querySelector('.cat-info');
+const errorEl = document.querySelector('.error');
+
+loadingEl.style.display = 'block';
+
+fetchBreeds(selectEl, loadingEl, errorEl);
+
+selectEl.addEventListener('change', handleSelectChange);
+
+async function handleSelectChange() {
+  const selectedBreedId = selectEl.value;
+
+  catInfoEl.style.display = 'none';
+  loadingEl.style.display = 'block';
+
+  try {
+    const catData = await fetchCatByBreed(
+      selectedBreedId,
+      selectEl,
+      loadingEl,
+      errorEl
+    );
+
+    createMarkup(catData);
+    catInfoEl.style.display = 'flex';
+  } catch (error) {
+    console.error(error);
+    alert('Oops! Something went wrong! please try again later.');
+  } finally {
+    loadingEl.style.display = 'none';
+  }
+}
+
+function createMarkup({ name, description, temperament, imageUrl }) {
+  const markup = `<img class="cat-img" style="object-fit: cover; border-radius: 16px" src="${imageUrl}" alt="${name}" width="400" height="300">
+  <div class="box" style="width: 500px; paddingLeft= 100px">
+    <h1>${name}</h1>
+    <p>${description}</p>
+    <h2>Temperament:</h2>
+    <p>${temperament}</p> 
+  </div>`;
+  catInfoEl.innerHTML = markup;
+  catInfoEl.style.boxShadow =
+    '0 2px 5px rgba(0, 0, 0, 0.2), 0 4px 6px rgba(0, 0, 0, 0.2)';
+}
